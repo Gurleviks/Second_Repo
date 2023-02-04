ed2fff37387d5aa0c472fb36775a33eb5a6d274c
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-02-04 15:44:26 +0300

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,19 @@
+class Animal:
+    def __init__(self, legs=4):
+        self.legs = legs
+        print("Animal object was created")
+
+    def run(self):
+        print("Running started")
+
+    def count_legs(self):
+        print("Number of legs:", self.legs)
+
+    def return_legs(self):
+        return self.legs
+
+animal = Animal()
+animal.run()
+animal.count_legs()
+print("Number of legs (direct):", animal.legs)
+print("Number of legs (returned):", animal.return_legs())
