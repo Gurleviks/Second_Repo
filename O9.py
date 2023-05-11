05e3c9a1e39ef1b8df7117ae18de0397072144dd
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-11 16:20:21 +0300

    Add files via upload

---
+++ O9.py
@@ -0,0 +1,27 @@
+a = 3
+b = 4
+
+integer_division = b // a
+print(integer_division)
+
+remainder = b % a
+print(remainder)
+
+num = [3, 5, 7, 9, 11]
+
+sum_num = sum(num)
+print(sum_num)
+
+
+grades = [83, 85, 72, 63, 95]
+
+ave_grade = sum(grades) / len(grades)
+print(ave_grade)
+
+
+prices = [5, 10, 15, 20, 25]
+
+high_price = max(prices)
+low_price = min(prices)
+print(high_price)
+print(low_price)
