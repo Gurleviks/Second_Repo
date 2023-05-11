80c9fe74f414e3a9e5f8f35c2ebb7e43abea8a33
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-11 16:07:16 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
-for i in range(1, 6):
-    if i % 2 == 0:
-        continue
-    print("*" * i)
+name = "Wololo"
+age = 24
+
+hello = f"Hello, my name is {name} and I am {age} years old."
+print(hello)
