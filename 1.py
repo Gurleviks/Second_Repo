13c04fe1560a89e1664794b18ae9f0a999a45918
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-11 16:04:35 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,4 +1,8 @@
-i = 1
-while i < 6:
-    print("*" * i)
-    i = i + 1
+a = "Laura and tom went ON a long walk."
+a = a.replace("ON", "on")
+a = a.replace("tom", "Tom")
+a = a.replace("long", "LONG")
+a = a.replace("a", "o")
+print(a)
+words = a.split()
+print(words)
