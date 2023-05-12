36f336506179d466357163fe07cf8dc14ec1351f
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-12 09:41:49 +0300

    Add files via upload

---
+++ V3.py
@@ -0,0 +1,22 @@
+#1
+name = "Wololo"
+age = 58
+city = "Sivas"
+#2
+sentence = "I am " + name + ", I am " + str(age) + " years old, and I live in " + city + "."
+print(sentence)
+#3
+name = "Mr. " + name
+#4
+sentence = "I am {}, I am {} years old, and I live in {}.".format(name, age, city)
+print(sentence)
+#5
+name = "Wololo"
+age = 58
+height = 1.82
+is_student = False
+#6
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
