c40e3099aaebb16d7668f6e0e3b04168a85b386a
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-12 09:30:55 +0300

    Add files via upload

---
+++ UDT.py
@@ -0,0 +1,18 @@
+integer_var = 58
+float_var = 58.58
+string_var = "Wololo!!!!"
+boolean_var = True
+
+print(type(integer_var))
+print(type(float_var))
+print(type(string_var))
+print(type(boolean_var))
+
+res1 = integer_var + integer_var
+print(res1)
+
+res2 = string_var + str(float_var)
+print(res2)
+
+res3 = string_var == boolean_var
+print(res3)
