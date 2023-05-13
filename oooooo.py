25cdffd4e06c15fc12862f7507a25f5f7caec153
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-13 19:24:51 +0300

    Add files via upload

---
+++ oooooo.py
@@ -0,0 +1,18 @@
+name = "Wololo"
+
+first_character = name[0]
+last_character = name[-1]
+
+middle_part = name[1:-1]
+
+string1 = "Wololo"
+string2 = "Ayoyo"
+new_string = string1 + " " + string2
+
+length = len(name)
+
+print(first_character)
+print(last_character)
+print(middle_part)
+print(new_string)
+print(length)
