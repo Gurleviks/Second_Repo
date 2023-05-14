fef951fc19c17c171b684cd7c1971ec8fb42c42c
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-14 14:09:28 +0300

    Add files via upload

---
+++ V4.py
@@ -0,0 +1,40 @@
+# 1.
+name = input("Your Name ? ")
+age = input("Your Age? ")
+city = input("Your City of residence? ")
+
+# 2.
+sentence = "Hello, I am " + name + ". I am " + age + " years old and Iam living in " + city + "."
+print(sentence)
+
+# 3.
+upper_name = name.upper()
+lower_city = city.lower()
+
+# 4.
+name_len = len(name)
+city_len = len(city)
+
+print("Name length:", name_len)
+print("City length:", city_len)
+
+# 5.
+first_name = "Wololo"
+last_name = "Ayoyo"
+full_name = first_name + " " + last_name
+print("Full name:", full_name)
+
+# 6
+fav_language = "SQL"
+fav_language_sentence = "My favorite programming language is " + fav_language + "."
+print(fav_language_sentence)
+
+# 7.
+current_year = 2023
+birth_year = 2000
+age = current_year - birth_year
+print("Age:", age)
+
+# 8.
+sen_len = len(sentence)
+print("Sentence length:", sen_len)
