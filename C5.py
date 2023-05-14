eb70d216cd77e68ae97a5ba63957983d3f919395
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-14 14:37:22 +0300

    Add files via upload

---
+++ C5.py
@@ -0,0 +1,46 @@
+# 1
+age = 20
+if age < 18:
+    print("You are a teenager")
+else:
+    print("You are an adult")
+
+# 2
+name = "John"
+city = "New York"
+if name == "John" or name == "Jane":
+    print("You are from", city)
+else:
+    print("You are not from", city)
+
+# 3
+password = "Galatasaray"
+if len(password) < 6:
+    print("The password is too short")
+else:
+    print("The password is long enough")
+
+# 4
+number = 7
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+# 5
+x = 58
+y = 85
+if x > y:
+    print("X is greater than Y")
+elif y > x:
+    print("Y is greater than X")
+
+# 6
+a = 57
+b = 58
+if a == b:
+    print("A is equal to B")
+else:
+    print("A is not equal to B")
