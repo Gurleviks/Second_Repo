798815494d4552c21149fbb2077ca8d905eb2a46
Gurleviks <119896367+Gurleviks@users.noreply.github.com>
2023-05-14 14:21:43 +0300

    Add files via upload

---
+++ V5.py
@@ -0,0 +1,16 @@
+# 1
+name = input("Your Name?: ")
+first_character = name[0]
+last_character = name[-1]
+print("First character:", first_character)
+print("Last character:", last_character)
+
+# 2
+if any(character.isdigit() for character in name):
+    print("There is digits.")
+else:
+    print("There isn't any digits.")
+
+# 3 ?????
+
+# 4 ?????
